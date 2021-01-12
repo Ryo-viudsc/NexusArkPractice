bec75d091aae0dbce252b33a7e0a7234677aec04
ryo_viudsc <54487445+Ryo-viudsc@users.noreply.github.com>
2021-01-12 15:59:00 -0800

    remove function implemented

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -1,22 +1,34 @@
 import React from 'react';
 import './checkout-item.styles.scss';
 
+import {connect} from 'react-redux'
+import {clearItemFromCart} from '../../redux/cart/cart.actions';
+//bind the action with the CheckoutItem here
+// export const clearItemFromCart = item =>({
+//   type: CartActionTypes.CLEAR_ITEM_FROM_CART,
+//   payload : item
+// });
 
+const CheckoutItem = ({cartItem, clearItem}) => {
+  
+  const { name, imageUrl, price, quantity} = cartItem; 
 
-const CheckoutItem = ({cartItem : {name, imageUrl, price, quantity }}) => (
-    <div
-      className='checkout-item'
-    >
+   return(
+  <div className='checkout-item'>
       <div className='image-container'>
           <img alt='item' src={`${imageUrl}`} />
       </div>
       <span className='name'>{name}</span>
       <span className='quantity'>{quantity}</span>
       <span className='price'>{price}</span>
-      <span className='remove-button'>&#10005;</span>
+      <span onClick={()=> clearItem(cartItem)} className='remove-button'>&#10005;</span>
     </div>
-)
+   )
+}
 
 
+const mapDispatchToProps = dispatch => ({
+  clearItem: item => dispatch(clearItemFromCart(item))
+})
 
-export default CheckoutItem;
+export default connect(null, mapDispatchToProps)(CheckoutItem);
